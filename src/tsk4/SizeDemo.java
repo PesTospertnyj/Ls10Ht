db65a1645c719041ed5b48be504a0eced6a3aa83
PesTospertnyj <gonartur@ukr.net>
2018-10-26 01:48:29 +0300

    Second commit

--- src/tsk4/SizeDemo.java
+++ src/tsk4/SizeDemo.java
@@ -2,19 +2,11 @@ package tsk4;
 
 public class SizeDemo {
 
-    public static final String XXS = "XXS";
-    public static final String XS = "XS";
-    public static final String S = "S";
-    public static final String M = "M";
-    public static final String L = "L";
-
     public static void main(String[] args) {
-
-
 
         Sizes.getDescription("Детский размер");
         Sizes sizes = Sizes.XXS;
-        switch(sizes){
+        switch (sizes) {
             case XXS:
                 System.out.println("Это детский разммер");
                 break;
--- src/tsk4/Sizes.java
+++ src/tsk4/Sizes.java
@@ -3,18 +3,13 @@ package tsk4;
 public enum Sizes {
     XXS(32), XS(34), S(36), M(38), L(40);
 
-    public String a;
+    int[] euroSizes = {32,34,36,38,40};
 
-    public int euroSize;
+    private int number;
 
-    //Sizes[] array = {XXS,XS,S,M,L};
-
-    Sizes(int i, euroSize) {
+    Sizes(int number) {
+        this.number = number;
     }
-
-    /*Sizes(euroSize){
-
-    }*/
     public static void getDescription(String a){
         System.out.println("Взрослый размер");
     }
